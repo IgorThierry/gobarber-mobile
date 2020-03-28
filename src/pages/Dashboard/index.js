7fa31cb5a6efaeca376142436b4d49f7d1e92883
Igor Thierry <igorthierry15@gmail.com>
2020-03-28 18:06:26 -0300

    feat:appointments list, cancel appointment

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,24 +1,52 @@
-import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {Alert} from 'react-native';
+import api from '~/services/api';
 
 import Background from '~/components/Background';
+import Appointment from '~/components/Appointment';
+import { Container, Title, List } from './styles';
 
-import { useDispatch } from 'react-redux';
-import { signOut } from '~/store/modules/auth/actions';
+export default function Dashboard() {
+    const [appointments, setAppointments] = useState([]);
 
-export default function Dashboard({ navigation }) {
-    const dispatch = useDispatch();
+    useEffect(() => {
+        async function loadAppointment() {
+            const response = await api.get('appointments');
+            setAppointments(response.data);
+        }
 
-    function handleSubmit() {
-        dispatch(signOut());
+        loadAppointment();
+    }, []);
+
+    async function handleCancel(id) {
+        const response = await api.delete(`appointments/${id}`);
+        setAppointments(
+            appointments.map((appointment) =>
+                appointment.id === id
+                    ? { ...appointment, canceled_at: response.data.canceled_at }
+                    : appointment
+            )
+        );
+
+        Alert.alert('OK!', 'Agendamento cancelado com sucesso!');
     }
 
     return (
         <Background>
-            <Text>Dashbosta</Text>
-            <TouchableOpacity onPress={handleSubmit}>
-                <Text>Sair</Text>
-            </TouchableOpacity>
+            <Container>
+                <Title>Agendamentos</Title>
+
+                <List
+                    data={appointments}
+                    keyExtractor={(item) => String(item.id)}
+                    renderItem={({ item }) => (
+                        <Appointment
+                            onCancel={() => handleCancel(item.id)}
+                            data={item}
+                        />
+                    )}
+                />
+            </Container>
         </Background>
     );
 }
--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import { View } from 'react-native';
-
+import { TouchableOpacity, Text } from 'react-native';
 
 import Background from '~/components/Background';
 
+import { useDispatch } from 'react-redux';
+import { signOut } from '~/store/modules/auth/actions';
 // import { Container } from './styles';
 
 export default function Profile() {
-    return <Background />;
+    const dispatch = useDispatch();
+
+    function handleSair() {
+        dispatch(signOut());
+    }
+
+    return (
+        <Background>
+            <TouchableOpacity onPress={handleSair}>
+                <Text>Sair</Text>
+            </TouchableOpacity>
+        </Background>
+    );
 }
