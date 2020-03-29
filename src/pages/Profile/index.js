4ca8d2974a5523b7d5befde80e9cea2ebfdb94bc
Igor Thierry <igorthierry15@gmail.com>
2020-03-29 11:21:34 -0300

    feat: user profile

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,24 +1,131 @@
-import React from 'react';
-import { TouchableOpacity, Text } from 'react-native';
+import React, { useRef, useState, useEffect } from 'react';
 
 import Background from '~/components/Background';
 
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { signOut } from '~/store/modules/auth/actions';
-// import { Container } from './styles';
+import { updateProfileRequest } from '~/store/modules/user/actions';
+
+import {
+    Container,
+    Title,
+    Form,
+    FormInput,
+    Separator,
+    SubmitButton,
+} from './styles';
 
 export default function Profile() {
+    const profile = useSelector((state) => state.user.profile);
     const dispatch = useDispatch();
 
-    function handleSair() {
+    const emailRef = useRef();
+    const oldPasswordRef = useRef();
+    const passwordRef = useRef();
+    const confirmPasswordRef = useRef();
+
+    const [name, setName] = useState(profile.name);
+    const [email, setEmail] = useState(profile.email);
+    const [oldPassword, setOldPassword] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+
+    useEffect(() => {
+        setOldPassword('');
+        setPassword('');
+        setConfirmPassword('');
+    }, [profile]);
+
+    function handleLogout() {
         dispatch(signOut());
+    }
+
+    function handleSubmit() {
+        dispatch(
+            updateProfileRequest({
+                name,
+                email,
+                oldPassword,
+                password,
+                confirmPassword,
+            })
+        );
     }
 
     return (
         <Background>
-            <TouchableOpacity onPress={handleSair}>
-                <Text>Sair</Text>
-            </TouchableOpacity>
+            <Container>
+                <Title>Meu perfil</Title>
+                <Form>
+                    <FormInput
+                        icon="person-outline"
+                        autoCorrect={false}
+                        autoCapitalize="none"
+                        placeholder="Nome completo"
+                        returnKeyType="next"
+                        onSubmitEditing={() => emailRef.current.focus()}
+                        value={name}
+                        onChangeText={setName}
+                    />
+                    <FormInput
+                        icon="mail-outline"
+                        keyboardType="email-address"
+                        autoCorrect={false}
+                        autoCapitalize="none"
+                        placeholder="Digite seu e-mail"
+                        ref={emailRef}
+                        returnKeyType="next"
+                        onSubmitEditing={() => oldPasswordRef.current.focus()}
+                        value={email}
+                        onChangeText={setEmail}
+                    />
+
+                    <Separator />
+
+                    <FormInput
+                        icon="lock-outline"
+                        secureTextEntry
+                        placeholder="Sua senha atual"
+                        ref={oldPasswordRef}
+                        returnKeyType="next"
+                        onSubmitEditing={() => passwordRef.current.focus()}
+                        value={oldPassword}
+                        onChangeText={setOldPassword}
+                    />
+
+                    <FormInput
+                        icon="lock-outline"
+                        secureTextEntry
+                        placeholder="Sua nova senha"
+                        ref={passwordRef}
+                        returnKeyType="next"
+                        onSubmitEditing={() =>
+                            confirmPasswordRef.current.focus()
+                        }
+                        value={password}
+                        onChangeText={setPassword}
+                    />
+
+                    <FormInput
+                        icon="lock-outline"
+                        secureTextEntry
+                        placeholder="Confirmação de senha"
+                        ref={confirmPasswordRef}
+                        returnKeyType="send"
+                        onSubmitEditing={handleSubmit}
+                        value={confirmPassword}
+                        onChangeText={setConfirmPassword}
+                    />
+
+                    <SubmitButton onPress={handleSubmit}>
+                        Atualizar perfil
+                    </SubmitButton>
+
+                    <SubmitButton color="#f64c75" onPress={handleLogout}>
+                        Sair
+                    </SubmitButton>
+                </Form>
+            </Container>
         </Background>
     );
 }
